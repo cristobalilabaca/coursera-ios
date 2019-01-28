acb77d7acea52f18bf95486fb657b7e647563302
cristobalilabaca <cilabaca1@uc.cl>
2019-01-28 02:17:42 -0300

    final

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
-var str = "Hello, playground"
+var numeros = 0...100
+
+for n in numeros{
+    if n % 5 == 0{
+        print("\(n) Bingo!!!")
+    }
+    if n % 2 == 0{
+        print("\(n) par!!!")
+    }else{
+        print("\(n) impar!!!")
+    }
+    if n >= 30 && n <= 40{
+        print("\(n) Viva Swift!!!")
+    }
+}
